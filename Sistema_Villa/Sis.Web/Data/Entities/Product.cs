d733ac8ac77efe66b0dc432efc5758a81dc1a6c6
cesarvillamilruiz <cesar.hades77@gmail.com>
2019-08-16 20:05:25 -0500

    Controlador con repositorio

--- Sistema_Villa/Sis.Web/Data/Entities/Product.cs
+++ Sistema_Villa/Sis.Web/Data/Entities/Product.cs
@@ -7,6 +7,9 @@
     {
         public int Id { get; set; }
 
+        [StringLength(50,ErrorMessage ="El campo {0} debe tener entre 1 y {1} caracteres")]
+        [Required]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -16,10 +19,10 @@
         public string ImageUrl { get; set; }
 
         [Display(Name = "Ultima Compra")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Ultima Venta")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Esta Disponible ?")]
         public bool IsAvailabe { get; set; }
---
+++ Sistema_Villa/Sis.Web/Data/SeedDb.cs
@@ -0,0 +1,45 @@
+﻿namespace Sis.Web.Data
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+    
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("Iphone");
+                this.AddProduct("Xiaomi 5");
+                this.AddProduct("Huawei");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(10000),
+                IsAvailabe = true,
+                Stock = this.random.Next(100)
+            });
+        }
+    }
+
+}
